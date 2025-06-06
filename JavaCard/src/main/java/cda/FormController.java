5514ae0b0865b5c91a4534e0abde9ca149d3a42d
Nimwe <floallheilly@gmail.com>
2025-06-06 10:21:44 +0200

    Ajout Zoning - Dispatching des fichiers de travail Javafx et FXML

--- JavaCard/src/main/java/cda/FormController.java
+++ JavaCard/src/main/java/cda/FormController.java
@@ -3,7 +3,7 @@ package cda;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class FormController {
 
     @FXML
     private void switchToPrimary() throws IOException {
